beca0c55486313feb39f8696b7caff5ba3931cc7
Shreyas Raorane <raorane@seas.upenn.edu>
2025-04-15 22:53:30 -0400

    topics added for occupancy grid and pose and scan

--- src/sbamp/scripts/rrt_node.py
+++ src/sbamp/scripts/rrt_node.py
@@ -2,11 +2,27 @@
 
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSDurabilityPolicy, QoSHistoryPolicy
 
 class RRTNode(Node):
     def __init__(self):
         super().__init__("rrt_node")
         self.get_logger().info("Python rrt_node has been started.")
+
+        self.declare_parameter('scan_topic', '/scan')
+        self.declare_parameter('pose_topic', '/ego_racecar/odom')
+        self.declare_parameter('occupancy_grid_topic', '/occupancy_grid')
+
+
+        occupancy_grid_qos_profile = QoSProfile(
+            reliability=QoSReliabilityPolicy.RELIABLE,
+            durability=QoSDurabilityPolicy.TRANSIENT_LOCAL,
+            depth=1
+        )
+        
+        # ADD CODE TO GET OCCUPANCY GRID AND POSE HERE
+
+    
 
 def main(args=None):
     rclpy.init(args=args)
