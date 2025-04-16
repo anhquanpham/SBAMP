7c3f21d110d7cee5723690f8b4b417c47a0e9751
Shreyas Raorane <raorane@seas.upenn.edu>
2025-04-15 20:17:29 -0400

    demo added to get a function from prof nadia's code

--- src/sbamp/scripts/sbamp_node.py
+++ src/sbamp/scripts/sbamp_node.py
@@ -3,10 +3,16 @@
 import rclpy
 from rclpy.node import Node
 
+from sbamp.ds_opt_py.sbamp_header import demo
+
 class SBAMPNode(Node):
     def __init__(self):
         super().__init__("sbamp_node")
         self.get_logger().info("Python sbamp_node has been started.")
+
+        # Call the demo function from sbamp_header.py
+        demo_result = demo()
+        self.get_logger().info(f"Demo result: {demo_result}")
 
 def main(args=None):
     rclpy.init(args=args)
